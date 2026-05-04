d0b28a781bd6b20f8dccbcd3c0e562956e89cf71
dashborr <danisarfas7@gmail.com>
2026-05-04 16:59:13 +0700

    menambahkan class buku dan public

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    // Mengembalikan referensi ke objek itu sendiri (buku&)
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this; // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
