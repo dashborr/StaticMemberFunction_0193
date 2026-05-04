8878d47a48b34a72bd006559bb13804924f23aa1
dashborr <danisarfas7@gmail.com>
2026-05-04 16:40:29 +0700

    menambahkan mahasiswa::mahasiswa dan void mahasiswa::cetak

--- constructionoverloading.cpp
+++ constructionoverloading.cpp
@@ -13,3 +13,24 @@ class mahasiswa {
     mahasiswa (int inim, string pnama);
     void cetak();
 };
+
+mahasiswa::mahasiswa(){}
+
+    mahasiswa::mahasiswa(int inim){
+    nim = inim;
+ }
+
+ mahasiswa::mahasiswa(string pnama){
+    nama = pnama;
+ }
+
+ mahasiswa::mahasiswa(int inim,string pnama){
+    nim = inim;
+    nama = pnama;
+ }
+
+ void mahasiswa::cetak(){
+
+    cout << "nim" << nim << endl;
+    cout << "nama" << nama << endl;
+ }
