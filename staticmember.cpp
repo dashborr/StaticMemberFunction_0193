4f1caf5928eb35b63fc756938619a906c364f20e
dashborr <danisarfas7@gmail.com>
2026-05-04 15:53:59 +0700

    menambahkan int dan void

--- staticmember.cpp
+++ staticmember.cpp
@@ -18,3 +18,11 @@ public:
         setID();
     }
 };
+
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 0;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+};
