407a0c608bdadef3267f336fa2d22a60e100475f
dashborr <danisarfas7@gmail.com>
2026-05-04 15:55:05 +0700

    menambahkan void

--- staticmember.cpp
+++ staticmember.cpp
@@ -26,3 +26,10 @@ int Mahasiswa::nim = 0;
 void Mahasiswa::setID() {
     id = ++nim;
 };
+
+void Mahasiswa::printAll() {
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
