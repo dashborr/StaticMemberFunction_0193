d8273f2e0f7442930bbf66c8fe2374ff0db09aea
dashborr <danisarfas7@gmail.com>
2026-05-04 16:39:05 +0700

    menambahkan class mahasiswa

--- constructionoverloading.cpp
+++ constructionoverloading.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa (int inim, string pnama);
+    void cetak();
+};
